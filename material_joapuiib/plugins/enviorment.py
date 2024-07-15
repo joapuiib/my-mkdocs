4f4540cce9f51860f9bca6d176866fcc0a9ecf58
Joan Puigcerver <joapuiib@gmail.com>
2024-07-15 16:02:04 +0200

    env plugin

--- material_joapuiib/plugins/enviorment.py
+++ material_joapuiib/plugins/enviorment.py
@@ -4,17 +4,10 @@ class EnviormentPlugin(BasePlugin):
 
     def __init__(self):
         self.enabled = True
-        self.total_time = 0
-        self.is_dirty = False
-
-        self.is_building = True
+        self.is_building = False
 
     def on_startup(self, *, command, dirty):
-        self.is_dirty = dirty
-
-    def on_serve(self, server, config, builder):
-        self.is_building = False
-        return server
+        self.is_building = command in ['build', 'gh-deploy']
 
     def on_page_context(self, context, page, config, nav):
         context['build'] = self.is_building
